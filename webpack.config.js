f94aecdc348389a098dcb7f783b48e1e19aad7a6
Astaton <orson108@hotmail.com>
2018-04-14 20:47:20 -0400

    restructuring the file layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
+const htmlWebpack = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
-		app: './src/script.js'
+		script: './src/script.js'
 	},
 	output: {
 		path: path.resolve(__dirname, '/'),
-		filename: 'dist.script.js'
+		filename: '[name].bundle.js'
 	},
 	module: {
 		rules: [{
@@ -32,10 +33,20 @@ module.exports = {
 			]
 		}]
 	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		inline: true,
+		stats: 'errors-only'
+	},
 	plugins: [
 		new webpack.ProvidePlugin({
 			$: 'jquery',
 			jQuery: 'jquery'
+		}),
+		new htmlWebpack({
+			template: path.join(__dirname, 'src', 'index.html'),
+			inject: 'body'
 		})
+
 	]
 };
