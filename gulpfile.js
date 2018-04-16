a80b362db1b18e454c8f51f19e310df3ce72e6fa
Astaton <orson108@hotmail.com>
2018-04-16 17:33:21 -0400

    rebuilt the project after changes to the html:

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ const gulp = require('gulp');
 const gulpAutoprefix = require('gulp-autoprefixer');
 const gulpHtmlMin = require('gulp-htmlmin');
 
-const src_html = 'public/*.html';
+const src_html = 'src/*.html';
 const src_css = 'src/*.css';
-const dest_html = '';
+const dest_html = 'public/';
 const dest_css = 'src/';
 
 
@@ -20,7 +20,7 @@ gulp.task('prefixes', function(){
 
 //minifies html files
 gulp.task('htmlMini', function(){
-	return gulp.src(src_html)
+	gulp.src(src_html)
 	.pipe(gulpHtmlMin({
 		collapseWhitespace:true,
 		minifyCSS: true
